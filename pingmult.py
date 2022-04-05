ad800bf7432462d0468deeca289f7071cae82be3
thiagochan <thiagoortega.ifpr@gmail.com>
2022-04-05 11:02:57 -0300

    ping múltiplo

--- pingmult.py
+++ pingmult.py
@@ -2,13 +2,14 @@ import os
 
 hosts = ["google.com", "8.8.8.8", "4.4.4.4", "brasil.com"]
 
-txt = open('venv/hosts.txt', 'w')
+txt = open('hosts.txt', 'w')
 for h in range(len(hosts)):
-
     txt.write(f'{hosts[h]}\n')
 txt.close()
 
-with open('venv/hosts.txt') as file:
+with open('hosts.txt') as file:
     dump = file.read()
+    dump = dump.splitlines()
 
-    print(dump)
+    for ip in dump:
+        os.system('ping '+ ip)
